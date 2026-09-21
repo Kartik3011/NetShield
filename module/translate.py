82176deefb81f7ad4f9e982dae90353aa895edf8
Kartik <149142169+Kartik3011@users.noreply.github.com>
2026-09-21 22:45:24 +0530

    Update translate.py

--- module/translate.py
+++ module/translate.py
@@ -1,12 +1,11 @@
-
 from openai import OpenAI
 import streamlit as st
 
 client = OpenAI(
-  base_url = "https://integrate.api.nvidia.com/v1",
-  api_key = st.secrets["NVIDIA_API_KEY"] 
+    base_url="https://integrate.api.nvidia.com/v1",
+    api_key=st.secrets["NVIDIA_API_KEY"]
 )
- 
+
 def trans(a):
     con = """You are an advanced language model capable of understanding and translating multiple languages. 
     Your task is to translate the provided text into English. Do not provide context or explanations. 
@@ -20,18 +19,19 @@ def trans(a):
     The text to translate is as follows:
     """ + str(a)
   
-completion = client.chat.completions.create(
-  model="meta/llama3-8b-instruct",
-  messages=[{"role":"user","content":con}],
-  temperature=0.2,
-  top_p=0.7,
-  max_tokens=1024,
-  stream=True
-)
-ss=""
+    completion = client.chat.completions.create(
+        model="meta/llama3-8b-instruct",
+        messages=[{"role":"user","content":con}],
+        temperature=0.2,
+        top_p=0.7,
+        max_tokens=1024,
+        stream=True
+    )
+    ss = ""
 
-for chunk in completion:
-  if chunk.choices[0].delta.content is not None:
-    print(chunk.choices[0].delta.content, end="")
-    ss+=chunk.choices[0].delta.content
+    for chunk in completion:
+        if chunk.choices[0].delta.content is not None:
+            print(chunk.choices[0].delta.content, end="")
+            ss += chunk.choices[0].delta.content
+            
     return ss
